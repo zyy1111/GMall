8859ac5b1e872184a705fd3f3f2842c1e9b5375a
carrie <yiyunzhang117@gmail.com>
2020-06-27 23:43:49 -0400

    First Commit

---
+++ gmall-product/src/test/java/com/example/gmall/product/GmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
